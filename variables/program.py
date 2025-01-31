6256fc1a505b3aeaf6da75ce08cb78dc02fa2a4a
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-01-31 09:23:19 -0700

    I_finished_my_python_program

---
+++ variables/program.py
@@ -0,0 +1,10 @@
+# Adelle Ponczoch, Variables Practice Python
+
+
+name = input("please tell me your name: ")
+
+print(name+ ", this is my practice program.")
+
+print("It doesn't do anything yet.")
+
+print("Hopefully it gives me full points though!")
