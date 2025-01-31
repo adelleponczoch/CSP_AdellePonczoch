f1d19e3ac2eeffcea9cf5a6b89fcd7d74ac6a3d0
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-01-31 09:11:52 -0700

    I_finished_my_Cpractice

---
+++ Cpractice/practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char name[] = "Adelle";
+int small_number = 6;
+int big_number = 244;
+char breakfast[] = "pancakes";
+char fav_color[] = "blue";
+char school_name[] = "ucas";
+int year = 2025;
+char eye_color[] = "blue";
+int age = 15;
+char fav_school_subject[] = "english";
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", small_number);
+    printf("%d\n", big_number);
+    printf("%s\n", breakfast);
+    printf("%s\n", fav_color);
+    printf("%s\n", school_name);
+    printf("%d\n", year);
+    printf("%s\n", eye_color);
+    printf("%d\n", age);
+    printf("%s\n", fav_school_subject);
+    return 0;
+}
