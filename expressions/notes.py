15fc253d6d5863da1da2ab2549d319795bd633ed
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-03 09:52:31 -0700

    expressions notes

---
+++ expressions/notes.py
@@ -0,0 +1,13 @@
+#Adelle Ponczoch, Notes Python
+#integers are numbers with no decimal points
+#floats are numbers with decimal points
+#algorithms are a set of instructions to complete a task
+print(10.5+5)
+print(10.5-5)
+print(10.5*5)
+print(10.5/5)
+print(10.5**5) #exponents
+print(10.5//5) #whole number division
+print(10.5%5) #modulo/modulus/MOD gives remainder
+
+print(3+7*2)
