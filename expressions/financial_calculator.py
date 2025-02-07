5642b8070a85b6a0fda91ddf59fe861cad211992
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-07 00:38:19 +0000

    worked on financial calc in python

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -13,21 +13,21 @@ groceries = float (input("What is your monthly grocery cost?\n"))
 # Ask for transportation expenses (user input)
 transportation = float (input("What is your monthly transportation cost?\n"))
 # Calculate savings as 10% of income (variable)
-
+savings = "10% of income"
 # Calculate spending money income - ( rent+ utilities+ groceries+ transportation+ savings) (variable)
-
+spending_money = income-(rent+ utilites+ groceries+ transportation+ savings)
 # Calculate percent of rent (rent/ income) (variable)
-
+percent_rent = rent/ income
 # Calculate percent of utilities (utilities/ income) (variable)
-
+percent_utilites = utilites/ income
 # Calculate percent of groceries (groceries/ income) (variable)
-
+percent_groceries = groceries/ income
 # Calculate percent of transportation (transportation/ income) (variable)
-
+percent_transportation = transportation/ income
 # Calculate percent of spending (spending/income) (variable)
-
+percent_spending = spending/income
 # tell user category spending amount AND percent for rent("You spend $XX.XX on rent and that is XX% of your income.") (print)
-
+print("Each month you spend rent on rent, which is percent_rent of your monthly income.")
 # tell user category spending amount AND percent for utilities ("You spend $XX.XX on utilities and that is XX% of your income.") (print)
 
 # tell user category spending amount AND percent for groceries ("You spend $XX.XX on groceries and that is XX% of your income.") (print)
