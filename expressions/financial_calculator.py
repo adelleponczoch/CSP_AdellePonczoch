8bb1d568dbd3ab198746e4ded768c00635862d9c
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-07 09:10:36 -0700

    I worked on it and need to fix an error still

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -1,7 +1,7 @@
 # Adelle Ponczoch, Financial Calculator Python
 
 # Write a print statement telling the user what the program is (budget calculator)
-print("Welcome to my program! This will be able to tell you your monthly spending amount on different expenses as well as their total percentage cost from your monthly income?")
+print("Welcome to my program! This will be able to tell you your monthly spending amount on different expenses as well as their total percentage cost of your monthly income.")
 # Ask for monthly income (user input)
 income = float (input("What is your monthly income?\n"))
 # Ask for rent amount (user input)
@@ -13,13 +13,13 @@ groceries = float (input("What is your monthly grocery cost?\n"))
 # Ask for transportation expenses (user input)
 transportation = float (input("What is your monthly transportation cost?\n"))
 # Calculate savings as 10% of income (variable)
-savings = "10% of income"
+savings = income* 0.1
 # Calculate spending money income - ( rent+ utilities+ groceries+ transportation+ savings) (variable)
-spending_money = income-(rent+ utilites+ groceries+ transportation+ savings)
+spending = income-(rent+ utilities+ groceries+ transportation+ savings)
 # Calculate percent of rent (rent/ income) (variable)
 percent_rent = rent/ income
 # Calculate percent of utilities (utilities/ income) (variable)
-percent_utilites = utilites/ income
+percent_utilites = utilities/ income
 # Calculate percent of groceries (groceries/ income) (variable)
 percent_groceries = groceries/ income
 # Calculate percent of transportation (transportation/ income) (variable)
@@ -29,11 +29,12 @@ percent_spending = spending/income
 # tell user category spending amount AND percent for rent("You spend $XX.XX on rent and that is XX% of your income.") (print)
 print("Each month you spend rent on rent, which is percent_rent of your monthly income.")
 # tell user category spending amount AND percent for utilities ("You spend $XX.XX on utilities and that is XX% of your income.") (print)
-
+print("Each month you spend utilities on utilities, which is percent_utilities of your monthly income.")
 # tell user category spending amount AND percent for groceries ("You spend $XX.XX on groceries and that is XX% of your income.") (print)
-
+print("Each month you spend groceries on groceries, which is percent_groceries of your monthly income.")
 # tell user category spending amount AND percent for transportation ("You spend $XX.XX on transportation and that is XX% of your income.") (print)
-
+print("Each month you spend transportation on transportation, which is percent_transportation of your monthly income.")
 # tell user category spending amount AND percent for spending ("You spend $XX.XX on spending and that is XX% of your income.") (print)
-
+print("Each month you spend spending on spending, which is percent_spending of your monthly income.")
 # tell user category spending amount AND percent for savings ("You spend $XX.XX on savings and that is 10% of your income.") (print)
+print("Each month you spend savings on savings, which is percent_savings of your monthly income.")
