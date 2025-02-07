9cfb03ec253e6a0e60140792801dc1f4e6c00622
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-07 09:39:42 -0700

    fixed issue on fincancial calc python, and worked on financial calc c

--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -1,7 +1,33 @@
 // Adelle Ponczoch, Financial Calculator C
 #include <stdio.h>
 
+// Ask for monthly income (user input)
+float income = [10];
+// Ask for rent amount (user input)
+float rent = [10];
+// Ask for utilities expenses (user input)
+float utilities = [10];
+// Ask for groceries expenses (user input)
+float groceries = (input("What is your monthly grocery cost?\n"));
+// Ask for transportation expenses (user input)
+float transportation = (input("What is your monthly transportation cost?\n"));
+// Calculate savings as 10% of income (variable)
+float savings = income* 0.1;
+// Calculate spending money income - ( rent+ utilities+ groceries+ transportation+ savings) (variable)
+float spending = income-(rent+ utilities+ groceries+ transportation+ savings);
+// Calculate percent of rent (rent/ income) (variable)
+float percent_rent = (rent/ income) *100;
+// Calculate percent of utilities (utilities/ income) (variable)
+float percent_utilities = (utilities/ income) *100;
+// Calculate percent of groceries (groceries/ income) (variable)
+float percent_groceries = (groceries/ income) *100;
+// Calculate percent of transportation (transportation/ income) (variable)
+float percent_transportation = (transportation/ income) *100;
+// Calculate percent of spending (spending/income) (variable)
+float percent_spending = (spending/income) *100;
+
 int main(void){
-    printf("Hello World");
+    printf("Welcome to my program! This will be able to tell you your monthly spending amount on different expenses as well as their total percentage cost of your monthly income.\n");
+
     return 0;
 }
