2e9d12be1c9ac6757f9b14beec2835c89b85e08f
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-12 09:40:12 -0700

    I finished my silly sentences for c!

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -2,7 +2,28 @@
 #include <stdio.h>
 #include <string.h>
 
+char name[10];
+
+char animal[15];
+
+char verb[10];
+
+char dessert[15];
+
+char time[10];
+
 int main(void){
-    printf("Hello World");
+    printf("Give me a name: ");
+    scanf("%s", name);
+    printf("Give me an animal: ");
+    scanf("%s", animal);
+    printf("Give me a present tense verb: ");
+    scanf("%s", verb);
+    printf("Give me a dessert: ");
+    scanf("%s", dessert);
+    printf("Give me a time of day: ");
+    scanf("%s", time);
+    
+    printf("Every day, %s the %s would %s for hours, or at least until their daily %s gorge at %s.", name, animal, verb, dessert, time);
     return 0;
 }
