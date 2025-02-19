42bb0b625adfc882e3f99af82f72056c22a34b28
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-19 09:49:46 -0700

    I finished the update of Hello World assignment with functions

---
+++ functions/update_of_hello_world.py
@@ -0,0 +1,11 @@
+# Adelle Ponczoch, Update of Hello World Python
+
+name = input("Please tell me your name: \n")
+def user(name):
+    print(f"Hello {name}!")
+
+user(name)
+user("Eve")
+user("Elizabeth")
+user("Faith")
+user("Paige")
