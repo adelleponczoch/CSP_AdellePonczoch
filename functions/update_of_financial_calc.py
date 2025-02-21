33fd5d4297b95d163dd16bfcc843f08931b5b92f
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-21 09:25:07 -0700

    I finished my update of financial calculator in python!

--- functions/update_of_financial_calc.py
+++ functions/update_of_financial_calc.py
@@ -1,31 +1,19 @@
 # Adelle Ponczoch, Update of Financial Calculator Python
 
+print("Welcome to my program! This will be able to tell you your monthly spending amount on different expenses as well as their total percentage cost of your monthly income.\n")
+
 def info(income, amount, type):
     percent_type = (amount/ income) *100
     print(f"Each month you spend ${amount:.2f} on {type}, which is {percent_type:.2f}% of your monthly income.")
 
-def user(amount, type):
-    type = float (input ("What is your monthly amount?\n"))
+def user(type):
+    return float (input (f"What is your monthly cost of {type}?\n"))
 
-user(income, rent, "rent")
-user(income, utilities, "utilities")
-user(income, groceries, "groceries")
-user(income, transportation, "transportation")
-
-
-# Write a print statement telling the user what the program is (budget calculator)
-print("Welcome to my program! This will be able to tell you your monthly spending amount on different expenses as well as their total percentage cost of your monthly income.\n")
-# Ask for monthly income (user input)
-income = float (input("What is your monthly income?\n"))
-# Ask for rent amount (user input)
-rent = float (input("What is your monthly rent?\n"))
-# Ask for utilities expenses (user input)
-utilities = float (input("What is your monthly utilities cost?\n"))
-# Ask for groceries expenses (user input)
-groceries = float (input("What is your monthly grocery cost?\n"))
-# Ask for transportation expenses (user input)
-transportation = float (input("What is your monthly transportation cost?\n"))
-# Calculate savings as 10% of income (variable)
+income = user("income")
+rent = user("rent")
+utilities = user("utilities")
+groceries = user("groceries")
+transportation = user("transportation")
 savings = income* 0.1
 
 info(income, rent, "rent")
