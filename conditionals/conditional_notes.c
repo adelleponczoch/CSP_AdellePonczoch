bad0faea7a74e4f385f601c62ebef8b5a2be216e
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-25 09:40:13 -0700

    finished conditional notes in C

--- conditionals/conditional_notes.c
+++ conditionals/conditional_notes.c
@@ -1,7 +1,29 @@
 //Adelle Ponczoch, Conditional Notes C
 #include <stdio.h>
+#include <string.h>
+
+char name[] = "Vienna";
+int num = 7;
 
 int main(void){
-    printf("Hello World");
+    if(strcmp(name, "Vienna") == 0){
+        printf("Hello Ms. LaRose. Welcome to class.\n");
+    }else{
+        printf("Hello %s, welcome to class!\n", name);
+    }
+// && = and
+// || = or
+// != = not
+    if(num > 5 && num < 10){
+        if(num == 7){
+            printf("%d is an unlucky number.\n", num);
+        }else{
+            printf("%d is a large single digit number\n", num);
+        }
+    }else if (num >10){
+        printf("%d is not a single digit number\n", num);
+    }else{
+        printf("%d is a small single digit number'n", num);
+    }
     return 0;
 }
