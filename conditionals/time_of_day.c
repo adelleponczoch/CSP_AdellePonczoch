299db787755f6b3629ac320cc4dafab37b63fd93
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-02-27 09:47:30 -0700

    I finished my time of day in c!

--- conditionals/time_of_day.c
+++ conditionals/time_of_day.c
@@ -1,7 +1,25 @@
 //Adelle Ponczoch, Template
 #include <stdio.h>
+#include <time.h>
 
 int main(void){
-    printf("Hello World");
+    time_t seconds;
+    seconds = time(NULL);
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    time_t now = time(NULL);
+    struct tm * tm_struct = localtime(&now);
+    int hour = tm_struct->tm_hour;
+
+    if(hour >= 12 && hour < 19){
+        printf("Good afternoon!");
+    }else if(hour >= 19){
+        printf("Good evening!");
+    }else{
+        printf("Good morning!");
+    }
     return 0;
 }
