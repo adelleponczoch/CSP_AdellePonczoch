fce569ed1787c2a0af984aa05323454bd3f515c4
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-03-03 09:40:44 -0700

    I finished my family loop in python and took python loop notes

--- loops/family_loop.py
+++ loops/family_loop.py
@@ -1,2 +1,6 @@
-# Adelle ponzoch, Loops Notes in Python
+# Adelle Ponzoch, Family Loop in Python
 
+print("Welcome to my program! This is going to say hello to each kid in my family.")
+siblings = ["Vienna", "Grace", "Jacob", "Seth", "Adelle", "Miriam", "Isaac", "Cyrus"]
+for sibling in siblings:
+    print("Hello", sibling)
--- loops/loops_notes.py
+++ loops/loops_notes.py
@@ -1,2 +1,40 @@
 # Adelle Ponczoch, Loop Notes in Python
 
+# Adelle Ponzoch, Loops Notes in Python
+
+# What is a loop? 
+    # a section of code that is going to repeat
+# What are the two types of loops?
+    #while loop
+x = 0
+while x < 10:
+    print("Hello", x)
+    x +=1
+
+    #for loop
+nums = [3,5,7,2,8]
+for num in nums:
+    print(num)
+# What is iteration
+    #when you do something multiple times, the act of repeating (version)
+
+# What are lists? 
+    #a bunch of values in the same variable
+siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tori", "Treyson", "Jeff", "Hailey"]
+#print one item from the list
+print(siblings[3])
+#change an item in a list
+siblings[7] = "Jake"
+
+#remove item in list
+siblings.pop(5)
+print(siblings)
+#proper list printing with a for loop
+for sibling in siblings:
+    print(sibling)
+# How do you make lists in python? 
+    #you make a variable, do brackets and put commas in between list items
+
+# How do you make for loops in python? 
+
+# How do you make while loops in python?
