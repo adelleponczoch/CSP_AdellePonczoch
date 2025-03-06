4d857f3ab535e3df5022814bf0bb1379232cde75
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-03-06 09:41:00 -0700

    finished fizzbuzz c

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,7 +1,21 @@
 //Adelle Ponczoch, FizzBuzz C
 #include <stdio.h>
 
+int x;
+
 int main(void){
-    printf("Hello World");
+    for(x =0; x <51; x++){
+        if(x%5 == 0 && x%3 == 0){
+            printf("FizzBuzz\n");
+            }
+        else if(x%5 == 0){
+            printf("Buzz\n");
+            }
+        else if(x%3 == 0){
+            printf("Fizz\n");
+        }else{
+            printf("%d\n", x);
+            }
+        }
     return 0;
 }
