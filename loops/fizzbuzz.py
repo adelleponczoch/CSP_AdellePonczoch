b986de39a65dcc1bc0e54129f49ab0509f8790d8
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-03-06 09:18:47 -0700

    I finished fizzbuzz in python and deleted exe files

--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,1 +1,12 @@
 # Adelle Ponczoch, Fizz Buzz in Python
+
+for num in range(1,51):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
+    
