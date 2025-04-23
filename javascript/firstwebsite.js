9759c65eafbf20e43ec630ad3d68d3d620507782
adelle.ponczoch <adelle.ponczoch@ucas-edu.net>
2025-04-23 16:03:46 -0600

    I fixed my first website

--- javascript/firstwebsite.js
+++ javascript/firstwebsite.js
@@ -1,7 +1,13 @@
 let images = ["https://ih1.redbubble.net/image.982959959.6041/flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "https://media.newyorker.com/photos/5a57ced6f686540bff451ef2/master/pass/180122_r31326.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZdTVOKsladTdQj3Oej2TWvsk5nfnky6ECQ&s"]
 
+let webpage = document.getElementById("hidden").style.display 
+
 function show(){
-    document.getElementById("hidden").style.display = "block"
+    if (document.getElementById("hidden").style.display == "block"){
+        document.getElementById("hidden").style.display = "none"
+    }else{
+        document.getElementById("hidden").style.display = "block"
+    }
 }
 count = 0
 function change(){
