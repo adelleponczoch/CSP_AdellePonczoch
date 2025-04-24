08957bff52c13f4368f6aee33ccab58e768e2b44
adelle.ponczoch <adelle.ponczoch@ucas-edu.net>
2025-04-24 09:46:50 -0600

    I fixed and have finished my first website!

--- javascript/notes.js
+++ javascript/notes.js
@@ -31,3 +31,12 @@ function show(){
 function pop(){
     window.alert("For real. Don't click this!")
 }
+function more(){
+        if (document.getElementById("extra").style.display != "flex"){
+            document.getElementById("extra").style.display = "flex"
+            document.getElementById("shw").innerHTML = "Show Less"
+    }else{
+            document.getElementById("extra").style.display = "none"
+            document.getElementById("shw").innerHTML = "Show More"
+    }
+}
