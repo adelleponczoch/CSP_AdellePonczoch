01713d008e065494ed488b713f91f9525fd998ef
adelleponczoch <adelle.ponczoch@ucas-edu.net>
2025-04-28 17:49:19 +0000

    worked on staycation project, pull next time!

--- javascript/final_staycation_project.js
+++ javascript/final_staycation_project.js
@@ -1,6 +1,23 @@
 let images = ["https://www.offthebeatenpath.com/wp-content/uploads/2021/08/entrance-zion-national-park.jpg", "https://media.timeout.com/images/106151559/750/562/image.jpg", "https://thegoldenhouradventurer.com/wp-content/uploads/2020/09/ObservationPointZionNP01-scaled.jpg"]
 
+let webpage = document.getElementById("text").style.display 
 
+function show(){
+    if (document.getElementById("text").style.display == "block"){
+        document.getElementById("text").style.display = "none"
+    }else{
+        document.getElementById("text").style.display = "block"
+    }
+}
+function show(){
+    if (document.getElementById("text").style.display != "flex"){
+        document.getElementById("text").style.display = "flex"
+        document.getElementById("btn").innerHTML = "Show Less"
+}else{
+        document.getElementById("text").style.display = "none"
+        document.getElementById("btn").innerHTML = "Show More"
+}
+}
 
 count = 0
 function change(){
